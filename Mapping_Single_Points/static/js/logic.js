a986f383116456d3f96981af1719837f92910fe8
sbenston <benstonstephanie@gmail.com>
2022-12-06 12:01:25 -0800

    Add code for mapping a point

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -4,16 +4,23 @@ console.log("working");
 // Create map object with a center and zoom level
 let map = L.map('mapid', {
     center: [
-        40.7, -94.5
+        34.0552, -118.2437
     ],
-    zoom: 4
+    zoom: 14
 });
 
 // Create tile layer that is background of map
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 // Add layer to the map
 streets.addTo(map);
+
+// Add marker for Los Angeles, California
+L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    color: '#000000',
+    fillColor: '#ffffa1'
+}).addTo(map);
